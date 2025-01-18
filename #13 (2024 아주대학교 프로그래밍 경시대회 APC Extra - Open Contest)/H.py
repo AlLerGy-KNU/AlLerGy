13470a01c273b2139b0f1060d41b896919a4b6fa
wychoi <soc09072@gmail.com>
2025-01-18 09:30:01 +0000

    아주대 H번

---
+++ #13 (2024 아주대학교 프로그래밍 경시대회 APC Extra - Open Contest)/H.py
@@ -0,0 +1,35 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+n = int(input().strip())
+
+graph = [[] for _ in range(n)]
+for _ in range(n-1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+# 그래프를 생성해줍니다. 
+
+dist = [-1] * n
+
+def bfs(start):
+    q = deque()
+    q.append(start)
+    dist[start] = 0
+    while q:
+        cur = q.popleft()
+        for nxt in graph[cur]:
+            if dist[nxt] == -1:
+                dist[nxt] = dist[cur] + 1
+                q.append(nxt)
+# 루트(0)부터 깊이를 기록해줍니다. 
+bfs(0)
+answer_bits = []
+for v in range(1, n):
+    bit = ((n-1) - dist[v]) % 2
+    answer_bits.append(str(bit))
+
+answer_bits.reverse()
+
+print("".join(answer_bits))
